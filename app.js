3481e2e24734a8b2a8f54dee70015deee59b626a
MusicoTeorico <miguelvigar@hotmail.com>
2025-02-28 22:12:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 let results = [];
 
+// Inicializa FlexSearch
+var index = new FlexSearch.Document({
+    encode: function (str) {
+        const cjkItems = str.replace(/[\x00-\x7F]/g, "").split("");
+        const asciiItems = str.toLowerCase().split(/\W+/);
+        return cjkItems.concat(asciiItems);
+    },
+    document: {
+        id: "id_str",
+        index: ["full_text"],
+        store: true
+    }
+});
+
 const searchInput = document.getElementById('search-input');
 const browseOutput = document.getElementById('browse-output');
 const loadingIndicator = document.getElementById('loading');
 const searchSection = document.getElementById('search');
 
+// Variables de paginación
+const pageSize = 50;
+let page = 1;
+let browseDocuments = [];
+
 // Procesar datos y agregar al índice de búsqueda
 function processData(data) {
+    browseDocuments = [...data].sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
     data.forEach(doc => {
         index.add({
             id_str: doc.id_str,
@@ -18,15 +38,9 @@ function processData(data) {
     });
     loadingIndicator.hidden = true;
     searchSection.hidden = false;
-    browseDocuments = [...data].sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
     renderBrowse();
     updatePagination();
 }
-
-// Variables de paginación
-const pageSize = 50;
-let page = 1;
-let browseDocuments = [];
 
 // Actualizar paginación
 function updatePagination() {
@@ -42,7 +56,7 @@ function renderBrowse() {
     const output = browseDocuments.slice(start, end).map(item => `
         <div class="tweet">
             <p class="search_item">
-                <div class="search_link"><a href="MusicoTeorico/status/${item.id_str}" target="_blank">link</a></div>
+                <div class="search_link"><a href="https://twitter.com/MusicoTeorico/status/${item.id_str}" target="_blank">Ver en Twitter</a></div>
                 <div class="search_text">${embedYouTubeLinks(item.full_text)}</div>
                 <div class="search_time">${new Date(item.created_at).toLocaleString()}</div>
             </p>
@@ -51,16 +65,12 @@ function renderBrowse() {
     `).join('');
     browseOutput.innerHTML = output;
     browseOutput.innerHTML += '<a href="#tabs">top &uarr;</a>';
-    embedYouTubeVideos();
 }
 
-
 // Incrustar videos de YouTube
-function embedYouTubeVideos() {
-    document.querySelectorAll(".tweet .search_text").forEach(textDiv => {
-        textDiv.innerHTML = textDiv.innerHTML.replace(/(https?:\/\/(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]+))/g, (match, url, videoId) => {
-            return `<br><iframe width="100%" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-        });
+function embedYouTubeLinks(text) {
+    return text.replace(/(https?:\/\/(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]+))/g, (match, url, videoId) => {
+        return `<br><iframe width="100%" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
     });
 }
 
@@ -88,6 +98,3 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('prev-page').addEventListener('click', goToPrevPage);
     processData(searchDocuments);
 });
-
-
-

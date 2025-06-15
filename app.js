41ceb98d08a32f8e4e2568c5420f898a867f9aae
MusicoTeorico <miguelvigar@hotmail.com>
2025-06-15 21:31:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -88,13 +88,13 @@ function renderResults() {
     return `
       <p class="search_item">
         <div class="search_link">
-          <a href="Sign_in_music/status/${item.id_str}" target="_blank">Ver tuit</a>
+          <a href="Musicoteorico/status/${item.id_str}" target="_blank">Ver tuit</a>
         </div>
         ${preview}
         <div class="search_text">${item.full_text}</div>
         <div class="search_time">${new Date(item.created_at).toLocaleString()}</div>
         <hr class="search_divider" />
-      </p>`.replace(/\.\.\/\.\.\/tweets_media\//g, 'Sign_in_music/tweets_media/');
+      </p>`.replace(/\.\.\/\.\.\/tweets_media\//g, 'MusicoTeorico/tweets_media/');
   });
   document.getElementById('output').innerHTML = output.join('');
   if (results.length > 0) {
@@ -157,13 +157,13 @@ function renderBrowse() {
     return `
       <p class="search_item">
         <div class="search_link">
-          <a href="Sign_in_music/status/${item.id_str}" target="_blank">Ver tuit</a>
+          <a href="MusicoTeorico/status/${item.id_str}" target="_blank">Ver tuit</a>
         </div>
         ${preview}
         <div class="search_text">${item.full_text}</div>
         <div class="search_time">${new Date(item.created_at).toLocaleString()}</div>
         <hr class="search_divider" />
-      </p>`.replace(/\.\.\/\.\.\/tweets_media\//g, 'Sign_in_music/tweets_media/');
+      </p>`.replace(/\.\.\/\.\.\/tweets_media\//g, 'MusicoTeorico/tweets_media/');
   });
   document.getElementById('browse-output').innerHTML = output.join('');
   document.getElementById('browse-output').innerHTML += '<a href="#tabs">top &uarr;</a>';
